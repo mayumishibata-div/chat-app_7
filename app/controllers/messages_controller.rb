804ada4e45a7e7306ed4a009219fbd775160e8c5
mayumishibata-div <mayumi.shibata@di-v.co.jp>
2023-08-18 12:25:24 +0900

    Active Storageの導入

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,7 +7,7 @@ class MessagesController < ApplicationController
 
   def create
     @room = Room.find(params[:room_id])
-    @message = @room.messages.new(message_params)
+    @message = 　@room.messages.new(message_params)
     if @message.save
       redirect_to  room_messages_path(@room)
     else 
@@ -18,6 +18,6 @@ class MessagesController < ApplicationController
 
   private
   def message_params
-    params.require(:message).permit(:content).merge(user_id: current_user.id)
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
 end
