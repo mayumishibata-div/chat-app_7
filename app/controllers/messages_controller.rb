e7d09df177fb175c09ca43d4fa14c7d98cc3e424
mayumishibata-div <mayumi.shibata@di-v.co.jp>
2023-08-23 08:32:26 +0900

    画像表示の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,9 +7,9 @@ class MessagesController < ApplicationController
 
   def create
     @room = Room.find(params[:room_id])
-    @message = 　@room.messages.new(message_params)
+    @message = @room.messages.new(message_params)
     if @message.save
-      redirect_to  room_messages_path(@room)
+      redirect_to room_messages_path(@room)
     else 
       @message = @room.messages.includes(:user)
       render :index, status: :unprocessable_entity
